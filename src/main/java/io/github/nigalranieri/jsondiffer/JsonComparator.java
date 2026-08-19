b6ec524f4b45d533d1dc55fe4547f9a603f461cd
NigalRanieri <59968412+NigalRanieri@users.noreply.github.com>
2026-08-19 13:10:00 +0200

    Refactor comparison internals

--- src/main/java/io/github/nigalranieri/jsondiffer/JsonComparator.java
+++ src/main/java/io/github/nigalranieri/jsondiffer/JsonComparator.java
@@ -1,17 +1,28 @@
 package io.github.nigalranieri.jsondiffer;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.github.nigalranieri.jsondiffer.internal.ComparisonOptions;
+import io.github.nigalranieri.jsondiffer.internal.comparison.ComparisonEngine;
+import io.github.nigalranieri.jsondiffer.internal.parser.JacksonJsonParser;
 import io.github.nigalranieri.jsondiffer.result.ComparisonResult;
 
 public final class JsonComparator {
 
   private final ComparisonOptions options;
+  private final JacksonJsonParser parser;
 
   JsonComparator(ComparisonOptions options) {
     this.options = options;
+    this.parser = new JacksonJsonParser();
   }
 
   public ComparisonResult compare(String expected, String actual) {
-    return JsonCompare.compare(expected, actual, options);
+
+    JsonNode expectedNode = parser.parse(expected);
+    JsonNode actualNode = parser.parse(actual);
+
+    ComparisonEngine engine = new ComparisonEngine(options);
+
+    return engine.compare(expectedNode, actualNode);
   }
 }
